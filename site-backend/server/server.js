a4eebddeb692c5b0862bcdcfdfa8d196c5273d86
asifhaider995 <asif.haider995@gmail.com>
2020-09-02 19:15:44 +0600

    backend

---
+++ site-backend/server/server.js
@@ -0,0 +1,67 @@
+// Copyright IBM Corp. 2016,2019. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+const loopback = require('loopback');
+const boot = require('loopback-boot');
+
+const app = module.exports = loopback();
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    const baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      const explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
+};
+
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
+
+console.log(Object.keys(app.models))
+
+app.models.user.find((error, result)=>{
+  if (result.length === 0) {
+    const user = {
+      email: 'asif@asif.com',
+      password: 'test',
+      username: 'asif',
+    };
+
+    app.models.user.create(user, (error, result) => {
+      console.log("Tried to create user", error, result)
+    });
+  }
+})
+
+app.models.user.afterRemote('create', (ctx, user, next) => {
+    console.log("New User: ", user);
+    app.models.Profile.create({
+      "first_name": user.username,
+      "created_at": new Date(),
+      "userId": user.id
+    }, (error, result) => {
+      if(!error && result) {
+        console.log("New Profile created ", result);
+      } else {
+        console.log(error)
+      }
+    })
+    next();
+})
